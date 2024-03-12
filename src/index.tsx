74bbf9ce1f24d02b2cdc7e6394418d326228c972
kento-yoshida <kento-yoshida@kke.co.jp>
2024-03-12 23:34:20 +0000

    Basic auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import { Hono } from 'hono'
+import { basicAuth } from "hono/basic-auth"
 import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
 
 import { renderer, AddTodo, Item } from './components'
 
 type Bindings = {
-  DB: D1Database
+  DB: D1Database,
+  USERNAME: string,
+  PASSWORD: string
 }
 
 type Todo = {
@@ -14,6 +17,14 @@ type Todo = {
 }
 
 const app = new Hono<{ Bindings: Bindings }>()
+
+app.use('*', async (c, next) => {
+  const auth = basicAuth({
+    username: c.env.USERNAME,
+    password: c.env.PASSWORD,
+  })
+  return auth(c, next)
+})
 
 app.get('*', renderer)
 
